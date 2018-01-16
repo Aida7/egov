dc639733531f807a088290edca4225f833b1c6df
Садыкова Аида Хаирбекова <@corp.itstep>
2018-01-16 12:01:21 +0600

    egov

---
+++ Egov/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using System.Net;
+using System.IO;
+
+
+
+namespace Egov
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var client = new WebClient().DownloadString("http://data.egov.kz/api/v2/valutalar_bagamdary4/v239?pretty");
+
+            List<ego> list = Newtonsoft.Json.JsonConvert.DeserializeObject<List<ego>>(client);
+            dataG.ItemsSource = list;
+        }
+        
+       
+    }
+}
---
+++ Egov/ego.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net;
+using System.IO;
+namespace Egov
+{
+    public class ego
+    { [JsonProperty("id")]
+        public int id { get; set; }
+        [JsonProperty("i")]
+        public int price  { get; set; }
+    }
+}
